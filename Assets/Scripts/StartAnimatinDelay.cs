f56c8b16a8d1c2d1f2b9514940462a2984b5a71e
Tamir Nachshon <tamirn7@gmail.com>
2020-12-27 10:26:36 +0200

    added some user interface

---
+++ Assets/Scripts/StartAnimatinDelay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimatinDelay : MonoBehaviour
+{   [Range(1,20)]
+    public float startDelay;
+    Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(DelayedAnimation());
+    }
+
+    // The delay coroutine
+    IEnumerator DelayedAnimation()
+    {
+        yield return new WaitForSeconds(startDelay);
+        animator.Play("Waiting");
+    }
+
+  
+}
--- Assets/Scripts/TowerFactory.cs
+++ Assets/Scripts/TowerFactory.cs
@@ -1,14 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerFactory : MonoBehaviour
 {
     [SerializeField] Tower towerPrefab;
     [SerializeField] int towerLimit = 5;
     [SerializeField] Transform towerParentTransform;
+    [SerializeField] Text TowerCount;
+    [SerializeField] Text TowerCountText;
 
-     Queue<Tower> towersQueue = new Queue<Tower>();
+
+    Queue<Tower> towersQueue = new Queue<Tower>();
+
+
+    private void Start()
+    {
+        TowerCount.text = (towerLimit -  towersQueue.Count).ToString();
+
+    }
 
     public void AddTower(WayPoint baseWayPoint)
     {
@@ -31,6 +43,21 @@ public class TowerFactory : MonoBehaviour
         baseWayPoint.isPlaceable = false;
         towersQueue.Enqueue(newTower);
         newTower.baseWayPoint = baseWayPoint;
+        HandleTowerCountText();
+
+    }
+
+    private void HandleTowerCountText()
+    {
+        if(towerLimit - towersQueue.Count != 0)
+        {
+            TowerCount.text = (towerLimit - towersQueue.Count).ToString();
+        }
+        else
+        {
+            TowerCount.text = "";
+            TowerCountText.text = "";
+        }
     }
 
     private void MoveExsitingTower(WayPoint newBaseWayPoint)
